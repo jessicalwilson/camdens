0870c6d7d48dde1f643119f35d638effd70c145a
Jessica Wilson <jessica.wilson@teamcolab.com>
2020-01-08 09:28:22 -0500

    home page acf logic

--- wp-content/themes/camden/front-page.php
+++ wp-content/themes/camden/front-page.php
@@ -1,33 +1,60 @@
 <?php get_header(); ?>
 
-<?php 
-	$hero_text = get_field('hero_text');
-	$hero_image = get_field('hero_image');
-?>
-
-<section class="hero hero--home" style="background-image: url(<?php echo $hero_image['url']; ?>)">
-	<div class="hero__copy">
-		<h2><?php echo $hero_text ?></h2>
-	</div>
-</section>
-
+<?php $hero = get_field('hero');?>
+	
+<?php if ($hero) :?>
+	<section class="hero hero--home" style="background-image: url(<?php echo $hero['hero_image']; ?>)">
+		<h2 class="hero__heading hero__heading--home"><?php echo $hero['hero_heading'] ?></h2>
+	</section>
+<?php endif; ?>
+	
 <main class="content home-content">
-	<section class="featured-items">
-		<h2 class="featured-items__title">Features</h2>
-		<?php ?>
-		<div></div>
+	
+	<?php $featured_items = get_field('featured_items');?>
+	
+	<?php if ( have_rows( 'featured_items' ) ) : ?>
+	<section class="section featured-items">
+		<h2 class="section-title">Features</h2>
+		<?php while( have_rows('featured_items') ): the_row(); ?>
+			<?php ?>
+			<div class="featured-item">
+				<img class="" src="" alt="">
+				<h3 class=""></h3>
+				<p class=""></p>
+			</div>
+		<?php endwhile; ?>
+	</section>
+	<?php endif; ?>
+	
+	<?php $about = get_field('about'); ?>
+	
+	<?php if ($about) :?>
+		<section class="section about">
+			<h2 class="section-title">About</h2>
+			<p><?php echo $about['about_story'] ?></p>
+			<img src="" alt="">
+		</section>
+	<?php endif; ?>
+	
+	<?php $callout = get_field('callout'); ?>
+	
+	<?php if ($callout) :?>
+		<section class="section callout">
+			<div class="callout__text"><?php echo $callout['callout_text'] ?></div>
+			<a href="<?php echo $callout['callout_link']['url'] ?>"><?php echo $callout['callout_link']['title'] ?></a>
+		</section>
+	<?php endif; ?>
+
+	<section class="section social-media">
+		<img src="" alt="">
+		<img src="" alt="">
+		<img src="" alt="">
 	</section>
 
-	<section class="images">
-		<img src="" alt="">
-		<img src="" alt="">
-		<img src="" alt="">
-	</section>
-
-	<section class="hours">
-		<h2>Hours</h2>
+	<section class="section hours">
+		<h2 class="section-title">Hours</h2>
 		<div class="course">
-			<h3></h3>
+			<h3 class=""></h3>
 		</div>
 		<div class="times">
 			<h4></h4>
@@ -35,4 +62,5 @@
 		</div>
 	</section>
 </main>
+
 <?php get_footer();
