d62e39619076da6bd2d4d57f5fbd1191d3081fff
Jessica Wilson <jessica.wilson@teamcolab.com>
2020-01-24 10:50:33 -0500

    Add support for ACF JSON

--- wp-content/themes/camden/functions.php
+++ wp-content/themes/camden/functions.php
@@ -11,15 +11,14 @@ add_theme_support( 'html5', [
 /** Allow WordPress to manage the document title */
 add_theme_support( 'title-tag' );
 
-/**
- * Register Menus
- */
+/** Register Menus */
 add_action('init', function () {
 	register_nav_menus([
 		'header' => __('Header Menu', 'custom'),
 	]);
 });
 
+/** Enqueue Styles & Scripts */
 function custom_wp_enqueue_scripts() {
 	wp_enqueue_style('typekit', 'https://use.typekit.net/ttx1qvt.css');
 	wp_enqueue_style('fontawesome', 'https://use.fontawesome.com/releases/v5.8.1/css/all.css');
@@ -31,3 +30,16 @@ function custom_wp_enqueue_scripts() {
 add_action('wp_enqueue_scripts', 'custom_wp_enqueue_scripts');
 
 add_filter( 'gform_confirmation_anchor', '__return_true' );
+
+/** Tell ACF where to store and pull local JSON files */
+function carbon_acf_json_save_point($path) {
+    return get_template_directory() . '/acf-json';
+}
+add_filter('acf/settings/save_json', 'carbon_acf_json_save_point');
+
+function carbon_acf_json_load_point($paths) {
+    unset($paths[0]);
+    $paths[] = get_template_directory() . '/acf-json';
+    return $paths;
+}
+add_filter('acf/settings/load_json', 'carbon_acf_json_load_point');
